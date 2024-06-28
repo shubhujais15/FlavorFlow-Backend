e8d2738f7cf7e8e70fdca24868b6433c7eda2eef
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-28 22:33:04 +0530

    Remove host name

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const corsAnywhere = require('cors-anywhere');
 
-const host = 'localhost';
+// const host = 'localhost';
 const port = 8080;
 
 corsAnywhere.createServer({
   originWhitelist: [], // Allow all origins
   requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
-  console.log(`CORS Anywhere server running on ${host}:${port}`);
+}).listen(port, () => {
+  console.log(`CORS Anywhere server running on :${port}`);
 });
